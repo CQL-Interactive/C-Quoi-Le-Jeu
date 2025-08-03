3c66665485235b40de5ac2b60d86171c19e1a448
CQuoiLeJeu-Dev <cquoilejeudev@gmail.com>
2025-08-03 18:56:40 +0200

    Modification de .gitinore

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sqlite3 = require('sqlite3').verbose()
 
 
 const app = express()
-dotenv.config()
+dotenv.config({ path: './config.env' })
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 

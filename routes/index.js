b86142f0df5c7052522c6d0249df0bba8f2298f7
CQuoiLeJeu-Dev <cquoilejeudev@gmail.com>
2025-08-05 21:01:02 +0200

    Merge branch 'main' of https://github.com/tmloff/C-Quoi-Le-Jeu-js into tmloff-main

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const path = require('path')
-
-router.use('/user', require(path.join(__dirname, 'api', 'user')))
-router.use('/auth', require(path.join(__dirname, 'api', 'auth')))
-router.use('/games', require(path.join(__dirname, 'api', 'games')))
-router.use('/game', require(path.join(__dirname, 'api', 'game')))
-
-module.exports = router

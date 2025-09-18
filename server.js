32b42c8cafdb13a421a30eaca095fd63e1cab6f1
C Quoi Le Jeu <cquoilejeudev@gmail.com>
2025-09-18 16:47:08 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         maxAge: 1000 * 60 * 60, // 1h
-        secure: true, // HTTPS ?
+        secure: false, // HTTPS ?
         httpOnly: true,
     }
 }))

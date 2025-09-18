087569976450e6073153389167f20855dbfa0a0d
C Quoi Le Jeu <cquoilejeudev@gmail.com>
2025-09-18 13:08:50 +0200

    Passage sur HTTPS

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         maxAge: 1000 * 60 * 60, // 1h
-        secure: false, // HTTPS ?
+        secure: true, // HTTPS ?
         httpOnly: true,
     }
 }))
